239de0fbb9e9ff5d4c51e1d68146218067835227
Simon Abrelat <simon.abrelat@gmail.com>
2018-01-11 21:42:44 -0500

    minimal alt PID

---
+++ src/org/usfirst/frc/team4468/robot/Util/PID.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team4468.robot.Util;
+
+public class PID {
+
+}
--- src/org/usfirst/frc/team4468/robot/Util/Paths/PathGeneration.java
+++ src/org/usfirst/frc/team4468/robot/Util/Paths/PathGeneration.java
@@ -27,30 +27,32 @@ public class PathGeneration {
      * and both represented by a common variable typically t.
      */
     public void generate() {
-        for(int i=0; i < W.length - 1; i++){
-            /* The first three rows are the general form of a quintic equation except that the real coefficient are 
-             * in another vector X where AX=B and the x in the general for is substituted with 0 because each function
-             * is in the range 0<=t<=1. The second three when you replace x with 1 showing the end of the function.
-             */
-            double[][] A = {
-                    {0, 0, 0, 0, 0, 1},
-                    {0, 0, 0, 0, 1, 0},
-                    {0, 0, 0, 2, 0, 0},
-                    {1 , 1 , 1, 1, 1, 1},
-                    {5 , 4 , 3, 2, 1, 0},
-                    {50, 12, 6, 2, 0, 0}
-            };
+        /* The first three rows are the general form of a quintic equation except that the real coefficient are 
+         * in another vector X where AX=B and the x in the general form is substituted with 0 because each function
+         * is in the range 0<=t<=1. The second three when you replace x with 1 showing the end of the function.
+         */
+        double[][] A = {
+                {0, 0, 0, 0, 0, 1},
+                {0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 2, 0, 0},
+                {1 , 1 , 1, 1, 1, 1},
+                {5 , 4 , 3, 2, 1, 0},
+                {50, 12, 6, 2, 0, 0}
+        };
         
-            /* append   gens       This Solves a systems of equations between all of the points creating two parameterized equations
-             * to the   a new      in the form AX=B where X = inv(A) * B where A is the value of X including shifts from the derivatives
-             * lists    poly       and the B is the outputs of all of the equations those being the Vel, Acc and Pos of the beginning 
-             * of       from       and end of the functions.
-             * polys    out                                                          |
-             * |        |    _______________________________________________________________________________________________________________
-             * \/       \/   |                                                                                                              |
+        A = Matrix.inverse(A);
+        
+        for(int i=0; i < W.length - 1; i++) {
+            /* append   gens   This Solves a systems of equations between all of the points creating two parameterized equations
+             * to the   a new  in the form AX=B where X = inv(A) * B where A is the value of X including shifts from the derivatives
+             * lists    poly   and the B is the outputs of all of the equations those being the Vel, Acc and Pos of the beginning 
+             * of       from   and end of the functions.
+             * polys    out                                                |
+             * |        |    ________________________________________________________________________________________________
+             * \/       \/   |                                                                                               |
              */
-            Y.add(new Quintic(Matrix.multiply(Matrix.inverse(A), new double[] { W[i].y, W[i].Vy, W[i].Ay, W[i+1].y, W[i+1].Vy, W[i+1].Ay })));
-            X.add(new Quintic(Matrix.multiply(Matrix.inverse(A), new double[] { W[i].x, W[i].Vx, W[i].Ax, W[i+1].y, W[i+1].Vx, W[i+1].Ax })));
+            Y.add(new Quintic(Matrix.multiply(A, new double[] { W[i].y, W[i].Vy, W[i].Ay, W[i+1].y, W[i+1].Vy, W[i+1].Ay })));
+            X.add(new Quintic(Matrix.multiply(A, new double[] { W[i].x, W[i].Vx, W[i].Ax, W[i+1].y, W[i+1].Vx, W[i+1].Ax })));
         }
         
     }
