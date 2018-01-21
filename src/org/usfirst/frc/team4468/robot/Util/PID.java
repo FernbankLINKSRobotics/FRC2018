6b52d7a492f99f936c7e4b3b7dba2092a1b7ec5c
mayarajan3 <mayarajan3@yahoo.com>
2018-01-20 19:29:24 -0500

    Motion Profiling function, pretty complete...

--- src/org/usfirst/frc/team4468/robot/Util/PID.java
+++ src/org/usfirst/frc/team4468/robot/Util/PID.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.Timer;
 public class PID {
     
     //Constants
+	public double maxVelocity = 6;
     private double kP;
     private double kI;
     private double kD;
@@ -98,26 +99,59 @@ public class PID {
         }
     }
     
-    public double[] motionProfiling(double endDistance, double v_cruise, double a, double time) {
-    	double velocity;
-    	double acceleration;
+    public double[] motionProfiling(double endDistance, double cruiseV, double a, double time, double cruiseRatio) {
+    	// The cruise distance is the total distance times the cruise ratio
+    	double v_cruise = Clamp(0, maxVelocity, cruiseV);
+    	double returnVelocity;
+    	double returnAcceleration;
     	double returnDistance;
-    	double accelDistance = (Math.pow(v_cruise, 2))/(2*a);
-    	if (accelDistance < (endDistance/2)*.95) {
-    		double timeTakenAD = Math.sqrt((2*accelDistance)/a);
-    		double cruiseTime = (endDistance - 2*accelDistance)*v_cruise;
+    	double accelDistance1 = (Math.pow(v_cruise, 2))/(2*a);
+    	if (accelDistance1 < (endDistance/2)*(1-(cruiseRatio/2))) {
+    		double timeTakenAD = Math.sqrt((2*accelDistance1)/a);
+    		double cruiseTime = (endDistance - 2*accelDistance1)/v_cruise;
     		double totalTime = 2*timeTakenAD + cruiseTime;
     		if (time <= timeTakenAD) {
     			returnDistance = (1/2)*a*Math.pow(time, 2);
+    			returnVelocity = a*time;
+    			returnAcceleration = a;
     		}
     		else if (time > timeTakenAD && time < (totalTime - timeTakenAD)) {
-    			returnDistance = accelDistance + (time-timeTakenAD)*v_cruise;
+    			double t = time - timeTakenAD;
+    			returnDistance = accelDistance1 + t*v_cruise;
+    			returnVelocity = v_cruise;
+    			returnAcceleration = 0;
     		}
     		else {
-    			returnDistance = accelDistance + cruiseTime*v_cruise + (1/2)*(a)*Math.pow((timeTakenAD - time), 2);
+    			double t = time - (cruiseTime+timeTakenAD);
+    			returnDistance = accelDistance1 + cruiseTime*v_cruise + (v_cruise*t+((1/2)*(-a)*Math.pow(t, 2)));
+    			returnVelocity = v_cruise + (-a)*t;
+    			returnAcceleration = -a;
     		}
     	}
-    	double[] array = {1.0, 2.0};
+    	else {
+    		double accelDistance2 = endDistance*((1-cruiseRatio)/2);
+    		double timeTakenAD = Math.sqrt((2*accelDistance2)/a);
+    		double cruiseTime = (endDistance*cruiseRatio)/v_cruise;
+    		double totalTime = timeTakenAD*2 + cruiseTime;
+    		if (time <= timeTakenAD) {
+    			returnDistance = (1/2)*a*Math.pow(time, 2);
+    			returnVelocity = a*time;
+    			returnAcceleration = a;
+    		}
+    		else if (time > timeTakenAD && time < (totalTime - timeTakenAD)) {
+    			double t = time - timeTakenAD;
+    			returnDistance = accelDistance2 + t*v_cruise;
+    			returnVelocity = v_cruise;
+    			returnAcceleration = 0;
+    		}
+    		else {
+    			double t = time - (cruiseTime+timeTakenAD);
+    			returnDistance = accelDistance2 + cruiseTime*v_cruise + (v_cruise*t+((1/2)*(-a)*Math.pow(t, 2)));
+    			returnVelocity = v_cruise + (-a)*t;
+    			returnAcceleration = -a;
+    		}
+    	}
+    	double[] array = {returnDistance, returnVelocity, returnAcceleration};
     	return array;
     }
     
