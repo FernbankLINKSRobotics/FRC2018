c1973552f10603a52145dc63d46610ae87d4f32c
mayarajan3 <mayarajan3@yahoo.com>
2018-02-19 20:21:59 -0500

    1d motion profiling implementation

--- Robot/src/org/usfirst/frc/team4468/robot/Commands/Manipulators/AngleRotate.java
+++ Robot/src/org/usfirst/frc/team4468/robot/Commands/Manipulators/AngleRotate.java
@@ -4,16 +4,24 @@ import org.usfirst.frc.team4468.robot.Constants;
 import org.usfirst.frc.team4468.robot.Robot;
 import org.usfirst.frc.team4468.robot.Subsystems.RotatingLift;
 import org.usfirst.frc.team4468.robot.Util.PID;
+import org.usfirst.frc.team4468.robot.Util.Paths.MotionProfiler;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class AngleRotate extends Command {
-    
+	double theta;
+	boolean negative;
     private RotatingLift rl = Robot.rotatingLift;
     private PID pid;
-
+    
     public AngleRotate(double angle) {
-        double theta = angle;
+        theta = angle;
+        if (angle<0) {
+        	negative = true;
+        }
+        else {
+        	negative = false;
+        }
         System.out.println("PID RotateAngle");
 
         pid = new PID(Constants.lifterP, Constants.lifterI, Constants.lifterD);
@@ -25,16 +33,18 @@ public class AngleRotate extends Command {
         pid.setPoint(theta);
         System.out.println("setpoint one: " + pid.getSetpoint());
     }
-
+    
+    private MotionProfiler onedMotion = new MotionProfiler(3.0, 5.0, 3.0, 0.1, theta, negative);
+    
     /*
      * Called repeatedly when this Command is scheduled to run (non-Javadoc)
      * 
      * @see edu.wpi.first.wpilibj.command.Command#execute()
      */
-    protected void execute() {
+    protected void execute(double time) {
         System.out.println("Executed");
-        rl.rotate(pid.calculate(rl.getAngle()));
-        System.out.println("Calculated Distance:" + pid.calculate(rl.getAngle()));
+        rl.rotate(pid.calculate(onedMotion.execute1D(time)[0]));
+        System.out.println("Calculated Distance:" + pid.calculate(onedMotion.execute1D(time)[0]));
     }
 
     /*
