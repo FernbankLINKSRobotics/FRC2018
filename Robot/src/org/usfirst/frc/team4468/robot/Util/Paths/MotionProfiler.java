04d6a08fb10e491c90e75ee2ef406842a12a805e
Taleb Hirani <talebhirani@Talebs-MacBook.local>
2018-03-13 16:56:30 -0400

    Updated 2d Motion Profiler

--- Robot/src/org/usfirst/frc/team4468/robot/Util/Paths/MotionProfiler.java
+++ Robot/src/org/usfirst/frc/team4468/robot/Util/Paths/MotionProfiler.java
@@ -153,55 +153,34 @@ public class MotionProfiler {
      * @return returnArray An array of the next angle, velocity, acceleration, and total angle that will be covered
      */
 	public double[] getDistance(double currentDist, double increment) {
-		double help = execute1D(0.0)[3];
-		double kale;
-		if (help>0.0) {
-			kale=help;
+		double kale = execute1D(0.0)[3];
+		double[] kaleDistance = new double[(int)(kale*100)+1];
+		double[] kaleVelocity = new double[(int)(kale*100)+1];
+		double[] kaleAcceleration = new double[(int)(kale*100)+1];
+		for (double i=0; i<kale;i=i+.01) {
+			kaleDistance[(int) (i*100)] = execute1D(i)[0];
+			kaleVelocity[(int) (i*100)] = execute1D(i)[1];
+			kaleAcceleration[(int) (i*100)] = execute1D(i)[2];
 		}
-		else {
-			kale =0.0;
-		}
-		if (kale == 0.0) {
-			double[] returnArray = {0.0,0.0,0.0,0.0};
-			return returnArray;
-		}
-		else {
-			double[] kaleDistance = new double[(int)(kale*100)+1];
-			double[] kaleVelocity = new double[(int)(kale*100)+1];
-			double[] kaleAcceleration = new double[(int)(kale*100)+1];
-			for (double i=0; i<kale;i=i+.01) {
-				kaleDistance[(int) (i*100)] = execute1D(i)[0];
-				kaleVelocity[(int) (i*100)] = execute1D(i)[1];
-				kaleAcceleration[(int) (i*100)] = execute1D(i)[2];
-			}
-			int l =0;
-			int currentIndex=0;
-				for (int i = 0; i < kaleDistance.length; i++) {
+		int l =0;
+		int currentIndex=0;
+		for (int i = 0; i < kaleDistance.length; i++) {
 			//finding where in the path the robot currently is based on current time
 			//This is where the array function below is used
-					if (currentDist > kaleDistance[l]) {
-						l++;
-					}
-					else {
-						if (l!=0) {
-							currentIndex = l-1;
-						}
-						else {
-							currentIndex = 0;
-						}
-					}
-				}
-				int index;
-				if ((currentIndex+(int)(increment*100))>((kale*100)+1)) {
-					index = (int) (kale*100);
+			if (currentDist > kaleDistance[l]) {
+				l++;
+			}
+			else {
+				if (l!=0) {
+					currentIndex = l-1;
 				}
 				else {
-					index = (currentIndex+(int)(increment*100));
+					currentIndex = 0;
 				}
-				double[] returnArray = {kaleDistance[index], kaleVelocity[currentIndex], kaleAcceleration[currentIndex], kaleDistance[kaleDistance.length-2]};
-				return returnArray;
+			}
 		}
-		
+		double[] returnArray = {kaleDistance[currentIndex+(int)(increment*100)], kaleVelocity[currentIndex], kaleAcceleration[currentIndex], kaleDistance[kaleDistance.length-2]};
+		return returnArray;
 	}
 	
 	/**
@@ -210,7 +189,7 @@ public class MotionProfiler {
      * @return An array of maximum velocity depending on curvature, distance covered (arc length), and the length of the chord between the arc
      */
 	public double[] getMaxVelocity(double x_current, double y_current, double x_curve, double y_curve, double x_end, double y_end) {
-		//These three inputs will come together to create an arc: an estimation of what the robot's motion will be
+		/*//These three inputs will come together to create an arc: an estimation of what the robot's motion will be
 		//Finding the intersection between the chord and the perpendicular line, which used x_curve and y_curve to be created
 		double x_inter = (x_end-x_current)/2.0;
 		double y_inter = (y_end-y_current)/2.0;
@@ -221,11 +200,77 @@ public class MotionProfiler {
 		
 		//finding the radius and arc length based on chord_height and chord_length
 		double radius = (chord_height/2.0)+(Math.pow(chord_length, 2)/(8.0*chord_height));
-		double distance = 2.0*Math.asin(-chord_length/(2.0*radius))*radius;
+		System.out.println(x_current+": "+x_curve+"; " + radius);
+		//double radius = (Math.pow((chord_length/2.0), 2)/(2*chord_height))+Math.pow(chord_height, 2);
+		double angle = 2*Math.acos((-chord_height/radius)+1);
+		double distance = radius*angle;
+		//2.0*Math.asin(chord_length/(2.0*radius));
 		//calculating maximum velocity considering curvature of the arc
 		double maxVelocity = Math.sqrt(radius*9.81);
-		double[] returnArray = {maxVelocity, Math.abs(distance), chord_length};
+		if (chord_height>chord_length) {
+			System.out.println("That's why...");
+		} */
+		double ax = x_current;
+		double ay = y_current;
+		double bx = x_curve;
+		double by = y_curve;
+		double cx = x_end;
+		double cy = y_end;
+		
+		double onecompa = 2.0*(cx-ax);
+		double onecompb = 2.0*(cy-ay);
+		double onecompc = -(Math.pow(ay, 2)+Math.pow(ax, 2)-Math.pow(cy, 2)-Math.pow(cx, 2));
+		
+		double twocompa = 2.0*(bx-ax);
+		double twocompb = 2.0*(by-ay);
+		double twocompc = -(Math.pow(ay, 2)+Math.pow(ax, 2)-Math.pow(by, 2)-Math.pow(bx, 2));
+		
+		double x_num = (onecompc*twocompb)-(twocompc*onecompb);
+		double x_den = (onecompa*twocompb)-(twocompa*onecompb);
+		double x = x_num/x_den;
+		
+		
+		double y_num = (onecompa*twocompc)-(twocompa*onecompc);
+		double y_den = (onecompa*twocompb)-(twocompa*onecompb);
+		double y = y_num/y_den;
+		
+		
+		double radius = Math.hypot((x_current-x), (y_current-y));
+		double distance;
+		double chord_length_one = Math.hypot(x_curve-x_current, y_curve-y_current);
+		double chord_length_two = Math.hypot(x_end-x_curve, y_end-y_curve);
+		double angle_one = Math.acos(((2*Math.pow(radius, 2))-Math.pow(chord_length_one, 2))/(2*Math.pow(radius, 2)));
+			//(2.0*radius*Math.asin(chord_length_one/(2.0*radius)))/(circ);
+		double angle_two = Math.acos(((2*Math.pow(radius, 2))-Math.pow(chord_length_two, 2))/(2*Math.pow(radius, 2)));
+		double distance_one = angle_one*radius;
+		double distance_two = angle_two*radius;
+		distance = distance_one+distance_two;
+		double maxVelocity = Math.sqrt(radius*9.81);
+		
+		
+		/*if (x_curve>x_current) {
+			left_one = false;
+		} else {
+			left_one = true;
+		} if (y_curve>y_current) {
+			back_one = false;
+		} else {
+			back_two = true;
+		} if (x_end>x_curve) {
+			left_two = true;
+		} else {
+			left_two = false;
+		} if (y_end>y_curve) {
+			back_two = true;
+		} else {
+			back_two = false;
+		}*/
+		
+		
+		
+		double[] returnArray = {maxVelocity, Math.abs(distance), chord_length_one};
 		return returnArray;
+
 	}
 	
 	/**
@@ -243,6 +288,8 @@ public class MotionProfiler {
 		double[] clampVel = new double[x_values.length+1];
 		double[] distance = new double[x_values.length];
 		double[] tempDistance = new double[x_values.length];
+		double[] testDistance = new double[x_values.length];
+		double[] tempTestDistance = new double[x_values.length];
 		double[] timeTaken = new double[x_values.length];
 
 		
@@ -257,7 +304,7 @@ public class MotionProfiler {
 					tempDistance[i+1] = (getMaxVelocity(x_values[i], y_values[i], x_values[i+1], y_values[i+1], x_values[i+2], y_values[i+2])[1])/2;
 					if (i < (allValues-2)) {
 						maxVelocities[i] = Clamp(0, max_velocity, (getMaxVelocity(x_values[i], y_values[i], x_values[i+1], y_values[i+1], x_values[i+2], y_values[i+2])[0]-limit));
-						tempDistance[i+2] = (getMaxVelocity(x_values[i], y_values[i], x_values[i+1], y_values[i+1], x_values[i+2], y_values[i+2])[1])/2;
+						tempDistance[i] = (getMaxVelocity(x_values[i], y_values[i], x_values[i+1], y_values[i+1], x_values[i+2], y_values[i+2])[1])/2;
 					}
 					//Use if statements so we don't get an error about array size
 					else {
@@ -299,7 +346,7 @@ public class MotionProfiler {
 		int accelIndex = 0;
 		for (int i = 1; i<x_values.length; i++) {
 			//Calculating velocity output after a specified distance of acceleration
-			double v = Math.sqrt(2*ta*arraySum(distance, y));
+			double v = Math.sqrt(2*ta*distance[y]);
 			//Checking if that velocity is significantly less than the maxVelocity set for that distance
 			if (v < maxVelocities[y]) {
 				y = y + 1;
@@ -310,7 +357,7 @@ public class MotionProfiler {
 			}
 		}
 		//SETTING ACCELERATION
-		for (int i = 0; i < (accelIndex+2); i++) {
+		for (int i = 0; i < (accelIndex+1); i++) {
 			if (acceleration[i] < ta) {
 				acceleration[i] = ta;
 			}
@@ -321,14 +368,14 @@ public class MotionProfiler {
 		
 		
 		//The velocities set below will override those previously set in the maxVelocities array based on acceleration, effectively clamping them
-    	for (int i = 1; i < (accelIndex+1); i++) {
+    	/*for (int i = 1; i < (accelIndex+1); i++) {
     		//Sets the current velocity during the start acceleration period
     		maxVelocities[i] = Clamp(Double.NEGATIVE_INFINITY, max_velocity, Math.sqrt(Math.pow(maxVelocities[i-1], 2)+2*acceleration[i-1]*(distance[i]-distance[i-1])));
-    	}
+    	}*/
     	
     	//SETTING DECELERATION
     	//We want this to be an overapproximation instead of an underapproximation so it fully goes to 0
-    	int decelIndex= accelIndex+2;
+    	int decelIndex= accelIndex+1;
     	for (int j=0; j<allValues; j++) {
     		for (int i = (allValues-decelIndex); i < (allValues); i++) {
     			//Sets the deceleration values
@@ -343,53 +390,125 @@ public class MotionProfiler {
     			}
     		}
     	}
+    	
+    	
     	//Changes the current velocity during the deceleration period
-    	if ((allValues-decelIndex-1)==0) {
+    	if ((allValues-decelIndex)==0) {
     		decelIndex = decelIndex-1;
     	}
     	for (int i = (allValues-decelIndex-1); i < (allValues); i++) {
-    		maxVelocities[i] = Clamp(Double.NEGATIVE_INFINITY, max_velocity, Math.sqrt(Math.pow(maxVelocities[i-1], 2)+2*acceleration[i]*(distance[i]-distance[i-1])));
+    		maxVelocities[i] = Clamp(Double.NEGATIVE_INFINITY, max_velocity, Math.sqrt(Math.pow(maxVelocities[i-1], 2)+2*acceleration[i-1]*(distance[i]-distance[i-1])));
+    		
     	}
+    	for (int i=0; i<allValues;i++) {
+    	}
+    	
     	
     	//Making sure the CHANGE of acceleration isn't greater than max acceleration specified
     	for (int i=1; i<allValues;i++) {
-    		double check;
-    		if (acceleration[i]<acceleration[i-1]) {
-    			check = Math.abs(acceleration[i-1]-acceleration[i]);
-    		}
-    		else {
-    			check = Math.abs(acceleration[i]-acceleration[i-1]);
-    		}
-    		if (check>max_acceleration) {
-    			for (int k=i; k<(i+1); k++) {
-    				acceleration[k] = (check/i)-acceleration[k-1];
+    		double check = acceleration[i]-acceleration[i-1];
+    		if (Math.abs(check)>max_acceleration) {
+    			int h=i-1;
+    			boolean hm = false;
+    			for (int m=0; m<allValues;m++) {
+    				for (int b=(h); b<(i+1);b++) {
+    					acceleration[b] = check/h;
+    					if (Math.abs(acceleration[b]-acceleration[i-1])>max_acceleration) {
+    						hm = true;
+    					}
+    					else {
+    						hm = false;
+    					}
+    				} if (hm) {
+						h=h-1;
+					}
     			}
     		}
-    		else {
-    			//do nothing	
-    		
-    		}
-
     	}
+    	
+    	double[] velocitiestwo = new double[allValues];
+    	double[] newAccel = new double[allValues];
+    	velocitiestwo[0]=0;
+    	if (maxVelocities[allValues-1]>0) {
+    		int l=1;
+    		double red = 0.0;
+    		for (int r=0; r<1000;r++) {
+    			for (int i=allValues-l; i<allValues; i++) {
+    			//(Math.abs((acceleration[i]-red)-acceleration[i-1])<max_acceleration) && 
+    				if (Math.abs((acceleration[i]-red)-acceleration[i-1])<max_acceleration && Math.abs(acceleration[i]-red)<max_acceleration) {
+    					newAccel[i]=acceleration[i]-red;  	
+    				}
+    				else {
+    					if (newAccel[i] < 100) {
+    						newAccel[i] = newAccel[i];
+    					}
+    					else {
+    						newAccel[i] = acceleration[i];
+    					}
+    				}
+    			}	
+    			for (int i=0; i<allValues-l;i++) {
+    				newAccel[i] = acceleration[i];
+    			}
+    			for (int m = 1; m < (allValues); m++) {
+    				velocitiestwo[m] = Clamp(Double.NEGATIVE_INFINITY, max_velocity, Math.sqrt(Math.pow(velocitiestwo[m-1], 2)+2*newAccel[m-1]*(distance[m]-distance[m-1])));
+    			}
+    			if (velocitiestwo[allValues-1]>.1) {
+    				if (l<(allValues-1)) {
+    					l=l+1;
+    				}
+    				else {
+    					red = red+.01;
+    					//l=1;
+    				}
+    				
+    			}
+    			else if (velocitiestwo[allValues-1]<=.1) {
+    			}
+    			else {
+    				red=red-.001;
+    			}
+    			
+    			
+    		}
+    		
+    	}
+    	
+    	for (int i=0; i<velocitiestwo.length;i++) {
+    		maxVelocities[i] = velocitiestwo[i];
+    		acceleration[i] = newAccel[i];
+    		System.out.println(maxVelocities[i] +", " + acceleration[i]);
+    	}
+    	
+    	
+    	/*for (int i=1; i<allValues; i++) {
+    		maxVelocities[i] = Clamp(Double.NEGATIVE_INFINITY, max_velocity, Math.sqrt(Math.pow(maxVelocities[i-1], 2)+2*acceleration[i-1]*(distance[i]-distance[i-1])));
+    		
+    	}*/
 		
+    	//Clamping velocity values
+    	for (int i=0; i<maxVelocities.length; i++) {
+    	    clampVel[i] = Clamp(Double.NEGATIVE_INFINITY, max_velocity, maxVelocities[i]);
+    	}
+    	
     	//Setting time
 		timeTaken[0] = 0;
     	for (int i = 1; i < x_values.length; i++) {
     		//Change in distance
     		double temp = distance[i] - distance[i-1];
-    		if (i<(allValues-1)) {
+    		if (i<(allValues)) {
     			if (acceleration[i-1] != 0) {
     				//the timeTaken array measures time COMPLETED whereas the velocity and acceleration array outputs FUTURE values (for the next increment)
     				//This is why we subtract i by 1 for velocity and acceleration
-    				timeTaken[i] = (2*temp)/(maxVelocities[i-1]+maxVelocities[i]);
+    				timeTaken[i] = (2*temp)/(clampVel[i-1]+clampVel[i]);
     			}
     			else {
     				//a reformat of d = v*t; there is no acceleration
-    				timeTaken[i] = temp/maxVelocities[i-1];
+    				timeTaken[i] = temp/clampVel[i];
     			}
     		}
     		else {
-    			timeTaken[i] = (2*temp)/(maxVelocities[i-1]);
+    			timeTaken[i] = (2*temp)/(clampVel[i-1]);
     		}
     	}
     	
@@ -422,25 +541,30 @@ public class MotionProfiler {
 		if (time > arraySum(timeTaken, timeTaken.length-1)) {
 			currentIndex = timeTaken.length-1;
 		}
-		//Clamping velocity values
-		for (int i=0; i<maxVelocities.length; i++) {
-    		clampVel[i] = Clamp(Double.NEGATIVE_INFINITY, max_velocity, maxVelocities[i]);
-    	}
 		
+		
+		
+		tempTestDistance[0]=0;
+		for (int i=1; i<allValues; i++) {
+			double timeDiff = timeTaken[i];
+			tempTestDistance[i] = (1.0/2.0)*acceleration[i-1]*Math.pow(timeDiff, 2)+clampVel[i-1]*timeDiff;
+		}
 		for (int i=0; i<allValues; i++) {
+			testDistance[i] = arraySum(tempTestDistance, i);
+			if (i==(allValues-1)) {
+				//System.out.println(Math.abs(distance[distance.length-1]-testDistance[i]) + " and " + testDistance[i]);
+			}
 			
 		}
 		
 		//calculating the different in time and distance (between current position and last increment)
 		double timeDifference = time - arraySum(timeTaken, currentIndex);
 		double distanceChange = Math.abs(clampVel[currentIndex]*timeDifference+(0.5)*acceleration[currentIndex]*Math.pow(timeDifference, 2));
-		//System.out.println("distance change: " + clampVel[currentIndex]*timeDifference + "; second part: " + (0.5)*acceleration[currentIndex]*Math.pow(timeDifference, 2));
+		//System.out.println("distance change: " + maxVelocities[currentIndex] + "; second part: " + (0.5)*acceleration[currentIndex]*Math.pow(timeDifference, 2));
 		double velocityChange = Math.abs(clampVel[currentIndex]*timeDifference+(0.5)*acceleration[currentIndex]*Math.pow(timeDifference, 2));
 		//System.out.println("current index: " + currentIndex + "; current distance: " + distance[currentIndex] + "; tiem difference: " + timeDifference);
 		//calculating total distance covered
-		returnDistance = distance[currentIndex] + distanceChange;
-	
-		
+		returnDistance = distance[currentIndex]+distanceChange;
 		
 		if (acceleration[currentIndex]!=0) {
 			returnAccel = acceleration[currentIndex];
@@ -471,7 +595,7 @@ public class MotionProfiler {
 		}
 		//stores what the distance covered, current velocity, and current acceleration values should be in an array
 		//stuff that begins with 'return' is from time, stuff that begins with 'current' is for location
-		double[] returnArray = {returnDistance, returnVelocity, returnAccel, arraySum(timeTaken, timeTaken.length-1)};
+		double[] returnArray = {returnDistance, returnVelocity, returnAccel, arraySum(timeTaken, timeTaken.length-1), distanceChange, time-arraySum(timeTaken, currentIndex-1), arraySum(timeTaken, currentIndex-1)};
 		return returnArray;
 	}
 	
@@ -481,9 +605,8 @@ public class MotionProfiler {
      * This is probably the function that will be used during autonomous (for the 2D Motion Profiling)
      * 
      * @param currentDist The instantaneous distance
-     * @param increment How far in the future to pull the distance
      * 
-     * @return returnArray An array of the next distance, velocity, acceleration, and total distance that will be covered
+     * @return returnArray An array of the current Velocity, acceleration, and total distance that will be covered
      */
 	public double[] getVelocity(double currentDist, double increment) {
 		//Getting max time to get to the end
@@ -554,4 +677,6 @@ public class MotionProfiler {
     	    return value;
     }
 	 
+    
+ 
 }
